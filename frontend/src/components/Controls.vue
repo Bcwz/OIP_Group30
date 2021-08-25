dcb0104a931b6abff5409b313da0c3eebf016b06
catkane-doodles <tanpeikai@hotmail.com>
2021-08-25 06:11:31 +0000

    basic socket set up

--- frontend/src/components/Controls.vue
+++ frontend/src/components/Controls.vue
@@ -1,20 +1,15 @@
 <template>
   <div>
     <div>
+      <div>
+        <p class="subtitle" v-if="running">Press button to stop</p>
+        <p class="subtitle" v-else>Press button to start</p>
+      </div>
       <button
-        v-if="!running"
         class="button is-primary is-fullwidth"
-        @click="send_signal(1)"
-      >
-        Start
-      </button>
-      <button
-        v-else
-        class="button is-danger is-fullwidth"
-        @click="send_signal(2)"
-      >
-        Stop
-      </button>
+        :class="{ 'is-primary': !running, 'is-danger': running }"
+        @click="send_signal()"
+      ></button>
     </div>
     <p>
       <span>{{ runtimeTranscription }}</span>
@@ -39,63 +34,61 @@ export default {
     };
   },
   methods: {
-    startSpeechRecognition() {
-      let SpeechRecognition =
-        window.SpeechRecognition || window.webkitSpeechRecognition;
+    // startSpeechRecognition() {
+    //   let SpeechRecognition =
+    //     window.SpeechRecognition || window.webkitSpeechRecognition;
 
-      if (!SpeechRecognition) {
-        this.error =
-          "Speech Recognition is not available on this browser. Please use Chrome or Firefox";
-        return;
-      }
+    //   if (!SpeechRecognition) {
+    //     this.error =
+    //       "Speech Recognition is not available on this browser. Please use Chrome or Firefox";
+    //     return;
+    //   }
 
-      let recognition = new SpeechRecognition();
-      recognition.lang = this.lang;
-      recognition.interimResults = true;
+    //   let recognition = new SpeechRecognition();
+    //   recognition.lang = this.lang;
+    //   recognition.interimResults = true;
 
-      recognition.addEventListener("result", (event) => {
-        const text = Array.from(event.results)
-          .map((result) => result[0])
-          .map((result) => result.transcript)
-          .join("");
-        this.runtimeTranscription = text;
-      });
+    //   recognition.addEventListener("result", (event) => {
+    //     const text = Array.from(event.results)
+    //       .map((result) => result[0])
+    //       .map((result) => result.transcript)
+    //       .join("");
+    //     this.runtimeTranscription = text;
+    //   });
 
-      recognition.addEventListener("end", () => {
-        if (this.runtimeTranscription !== "") {
-          if (this.runtimeTranscription.includes("start")) {
-            this.send_signal(1);
-          }
-          if (this.runtimeTranscription.includes("stop")) {
-            this.send_signal(2);
-          }
-          console.log(this.runtimeTranscription);
-        }
-        this.runtimeTranscription = "";
-        this.startSpeechRecognition();
-      });
+    //   recognition.addEventListener("end", () => {
+    //     if (this.runtimeTranscription !== "") {
+    //       if (this.runtimeTranscription.includes("start")) {
+    //         this.send_signal(1);
+    //       }
+    //       if (this.runtimeTranscription.includes("stop")) {
+    //         this.send_signal(2);
+    //       }
+    //       console.log(this.runtimeTranscription);
+    //     }
+    //     this.runtimeTranscription = "";
+    //     this.startSpeechRecognition();
+    //   });
 
-      recognition.start();
-    },
+    //   recognition.start();
+    // },
     set_state(val) {
       this.$store.state.state = val;
     },
-    send_signal(val) {
-      if (val == 1 && !this.running) {
+    send_signal() {
+      if (!this.running) {
         alert("Starting");
         this.set_state(constants.CLEANING);
         this.running = true;
-      } else if (val == 2 && this.running) {
+      } else if (this.running) {
         alert("Stopping");
         this.set_state(constants.IDLE);
         this.running = false;
-      } else {
-        alert("Invalid command");
       }
     },
   },
   mounted() {
-    this.startSpeechRecognition();
+    // this.startSpeechRecognition();
   },
 };
 </script>
---
+++ server/server_socket.py
@@ -0,0 +1,24 @@
+import eventlet
+import socketio
+
+sio = socketio.Server()
+# the index.html file hosted by eventlet is a dummy file
+# it appears to be required to host some html file.. 
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.on('msg')
+def message(sid, data):
+    print('message ', data)
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
