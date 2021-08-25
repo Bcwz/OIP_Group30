f1f33c153da676bff52577f0b73610de64640f06
catkane-doodles <tanpeikai@hotmail.com>
2021-08-25 07:00:05 +0000

    testing on pi3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import { createStore } from "vuex";
 import router from "./router";
 
+// import { io } from "socket.io-client";
+
 import App from "./App.vue";
 const store = createStore({
   state() {
@@ -14,4 +16,7 @@ const app = createApp(App);
 
 app.use(store);
 app.use(router);
+// app.config.globalProperties.$socket = io("https://services.peikai.pii.at/", {
+//   path: "/OIP-Server/",
+// });
 app.mount("#app");
