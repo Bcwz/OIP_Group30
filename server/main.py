dfe52d98049d76e3b30dcc1638f8cf2a23a1a385
Bernie <bernie.c.weizhuang@gmail.com>
2021-08-27 15:49:12 +0800

    Latest

--- server/main.py
+++ server/main.py
@@ -4,6 +4,10 @@ import socketio
 import board
 import adafruit_dht
 
+# For pi to arduino imports
+import serial
+import time
+
 import Constants
 
 from Utils import *
@@ -42,26 +46,52 @@ def get_temperature() -> str:
 
 def wash() -> None:
     # Insert command to start washing here
-    log("washing")
-    set_state(Constants.WASHING)
+    log("wash")
+    if state != Constants.STOP:
+        set_state(Constants.WASHING)
+        
+        signal = 1
+        ser.write(str(signal).encode('utf-8'))
+
+        time.sleep(1)
+    else:
+        log("wash", "Stopping wash because of state")
+
 
 
 def dry() -> None:
     # Insert command to start drying here
-    log("drying")
-    set_state(Constants.DRYING)
+    log("dry")
+    if state != Constants.STOP:
+        set_state(Constants.DRYING)
 
+        signal = 2
+        ser.write(str(signal).encode('utf-8'))
 
-def start_cleaning() -> None:
-    log("start_cleaning")
-    wash()
-    dry()
+        time.sleep(1)
+    else:
+        log("dry", "Stopping dry because of state")
 
 
 def stop_cleaning() -> None:
     log("stop_cleaning")
     set_state(Constants.STOP)
-    pass
+    signal = 0
+    ser.write(str(signal).encode('utf-8'))
+
+
+def start_cleaning() -> None:
+    log("start_cleaning")
+    set_state(Constants.START)
+
+    time = 0
+    while (state != Constants.STOP and time < 60):
+        if time < 15:
+            wash()
+        elif time < 60:
+            dry()
+        time += 1
+    stop_cleaning()
 
 
 def handle_(instructions) -> None:
@@ -73,9 +103,9 @@ def handle_(instructions) -> None:
     elif instructions == Constants.DRY:
         dry()
 
+
+
 # Start Listening
-
-
 @sio.on('connect')
 def connect(sid, environ) -> None:
     sio.enter_room(sid, clients)
@@ -96,5 +126,7 @@ def disconnect(sid):
 
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app)
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
 
+    eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app)
